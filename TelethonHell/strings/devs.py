ebdf683166bb1f2cabf5afa92755e662c20ba4f8
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-04-01 00:07:38 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

--- TelethonHell/strings/devs.py
+++ TelethonHell/strings/devs.py
@@ -1,9 +1,5 @@
 DEVLIST = [
-    "5672065955",   # Miss Pro
-    "1432756163",   # ForGo10God
-    "1874070588",   # ForGo10_God
-    "1533682758",   # Harry
-    "1344569458",   # iamgojoof6eyes
+    "7117906157",   # SAIF_DICTATOR
 ]
 
 HARMFUL = [
