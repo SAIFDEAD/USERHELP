2ba90e09b2a59ef12244cf9c4f3ae45ac52fc629
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-04-01 00:20:00 +0530

    Update __init__.py

--- TelethonHell/strings/__init__.py
+++ TelethonHell/strings/__init__.py
@@ -3,4 +3,4 @@ from TelethonHell.strings.fun_str import *
 from TelethonHell.strings.meme_str import *
 from TelethonHell.strings.others import *
 from TelethonHell.strings.quotes import *
-from TelethonHell.strings.rands import *
+from TelethonHell.strings.rands import 
